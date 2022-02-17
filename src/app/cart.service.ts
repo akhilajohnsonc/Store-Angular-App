5e2053c579518cad27a98fccdd4e2d8c9d9c1faf
akhilajohnsonc <59962882+akhilajohnsonc@users.noreply.github.com>
2022-02-17 19:53:22 +0530

    shipping prices from a external source

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import {Product} from './products';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   items: Product[] =[];
-  constructor() { }
+  constructor(private http: HttpClient) { }
   addToCart(product : Product){
     this.items.push(product);
   }
@@ -16,5 +16,9 @@ export class CartService {
   clearCart(){
     this.items =[];
     return this.items;
+  }
+  getShippingPrices() {
+    return this.http.get<{type: string,price: number}[]>
+    ('/assets/shipping.json');
   }
 }
