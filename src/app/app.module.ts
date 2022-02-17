e4e05e2e3c39e76594054b751058681dbb4d32ef
akhilajohnsonc <59962882+akhilajohnsonc@users.noreply.github.com>
2022-02-17 19:06:31 +0530

    Navigation changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,9 +14,8 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent 
-    
-      },
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent},
     ])
   ],
   declarations: [
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  product : Product | undefined;
+  /* ... */
+  constructor(private route:ActivatedRoute) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    const routparameters= this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routparameters.get('productId'));
+    this.product=products.find(product =>product.id === productIdFromRoute);
   }
 
 }
